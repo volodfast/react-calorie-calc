3d615b7e6a207d7a7bd453812a813591b08fe307
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-07-15 22:50:37 +0300

    feature(core): initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CallorieForm from './components/callorieForm';
+
+function App() {
+  return (
+    <div>
+      <CallorieForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/callorieForm/callorieForm.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const CallorieForm: FC = () => {
+  return <div>hello</div>;
+};
+
+export default CallorieForm;
