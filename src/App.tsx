e7ef4c0dd262f531caeeb838a44ca69cd3043a3b
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-07-17 20:19:55 +0300

    feature(core): add calorie calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Container } from '@material-ui/core';
+// containers
+import CalorieCalculator from 'modules/calorie/containers/CalorieCalculator';
 import AddProductForm from 'modules/product/containers/AddProductForm';
 
 function App() {
   return (
     <Container maxWidth="sm">
       <AddProductForm />
+      <CalorieCalculator />
     </Container>
   );
 }
---
+++ src/modules/calorie/containers/CalorieCalculator/CalorieCalculator.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CalorieCalculator = () => {
+  return <div>CalorieCalculator</div>;
+};
+
+export default CalorieCalculator;
