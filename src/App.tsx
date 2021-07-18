9937c836b939e49ee4ef25650025dc48df7cc7cf
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-07-18 22:26:31 +0300

    feature(core): remove add product form from index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Container } from '@material-ui/core';
 // containers
 import CalorieCalculator from 'modules/calorie/containers/CalorieCalculator';
-import AddProductForm from 'modules/product/containers/AddProductForm';
 
 function App() {
   return (
     <Container maxWidth="sm">
-      <AddProductForm />
       <CalorieCalculator />
     </Container>
   );
