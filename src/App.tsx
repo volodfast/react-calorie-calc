b147302cf4a5d7f843e9bcd20aa4c3882c144b10
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-07-19 22:35:16 +0300

    feature(core): add meal calorie calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@material-ui/core';
 // containers
 import CalorieCalculator from 'modules/calorie/containers/CalorieCalculator';
+import MealCalorieCalculator from 'modules/meal/containers/MealCalorieCalculator';
 
 function App() {
   return (
     <Container maxWidth="sm">
       <CalorieCalculator />
+      <MealCalorieCalculator />
     </Container>
   );
 }
---
+++ src/modules/meal/containers/MealCalorieCalculator/MealCaloriCalculator.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const MealCaloriCalculator: FC = () => {
+  return <div>Meal Calorie Calculator</div>;
+};
+
+export default MealCaloriCalculator;
